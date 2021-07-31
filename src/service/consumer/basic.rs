1f1fe15c24d4cda2af82e942466c8791bfa88a3a
StephenLing <lingfromsh@163.com>
2021-07-31 23:44:43 +0800

    Add service modules

---
+++ src/service/consumer/basic.rs
@@ -0,0 +1,38 @@
+use actix_web::{post, put, patch, web, Result, HttpResponse};
+use crate::model::consumer::basic::{ConsumerBasicModel};
+use crate::model::consumer::r#in::{ConsumerRegisterInputModel, ConsumerLoginInputModel, ChangePasswordInputModel, ResetPasswordInputModel, ModifyConsumerInfoInputModel};
+
+
+#[put("/")]
+async fn modify_info(new_info: web::Json<ModifyConsumerInfoInputModel>) -> Result<HttpResponse>{
+	println!("New consumer info: {:?}", new_info);
+	let modified = ConsumerBasicModel::new(0, None, None, None, None);
+	Ok(HttpResponse::Ok().json(modified))
+}
+
+#[post("/register/")]
+async fn register(new_consumer: web::Json<ConsumerRegisterInputModel>) -> Result<HttpResponse> {
+	println!("Register a new consumer: {:?}", new_consumer);
+	let registered = ConsumerBasicModel::new(0, None, None, None, None);
+	Ok(HttpResponse::Ok().json(registered))
+}
+
+#[post("/login/")]
+async fn login(consumer: web::Json<ConsumerLoginInputModel>) -> Result<HttpResponse> {
+	println!("Consumer Login: {:?}", consumer);
+	let logined = ConsumerBasicModel::new(0, None,None,None,None);
+	Ok(HttpResponse::Ok().json(logined))
+}
+
+#[put("/password/")]
+async fn reset_password(new_passowrd: web::Json<ResetPasswordInputModel>) -> Result<HttpResponse> {
+	println!("New password: {:?}", new_passowrd);
+	Ok(HttpResponse::Ok().body("Reset successfully!"))
+}
+
+#[patch("/password/")]
+async fn change_password(new_passowrd: web::Json<ChangePasswordInputModel>) -> Result<HttpResponse> {
+	println!("New password: {:?}", new_passowrd);
+	Ok(HttpResponse::Ok().body("Change password successfully!"))
+}
+
---
+++ src/service/mod.rs
@@ -0,0 +1,5 @@
+pub async fn default_api() -> &'static str {
+	"Welcome to flash-sale 's open api."
+} 
+
+pub mod consumer;
