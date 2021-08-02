57963d91bee11d1fe6ce03a7dd9f388eac407c8b
StephenLing <lingfromsh@163.com>
2021-08-02 15:23:45 +0800

    Add test for mongodb connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use actix_web::{web, App, HttpServer};
 
+mod database;
 mod model;
 mod service;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
+    println!("Server on.");
+    let client = database::get_mongodb_client();
+    let database_names = client.list_database_names(None, None).await.expect("List failed.");
+    println!("{:?}", database_names);
+
     HttpServer::new(
         || {
             App::new()
