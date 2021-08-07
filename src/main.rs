5f4451ba2fdb11adc52dcf02ff9880a5b1e94016
StephenLing <lingfromsh@163.com>
2021-08-07 23:20:14 +0800

    Remove useless

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
 use actix_web::{web, App, HttpServer};
 
+mod dao;
 mod database;
 mod model;
 mod service;
+mod security;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
-    println!("Server on.");
-    let client = database::get_mongodb_client();
-    let database_names = client.list_database_names(None, None).await.expect("List failed.");
-    println!("{:?}", database_names);
-
+async fn main() -> std::io::Result<()> {
     HttpServer::new(
         || {
             App::new()
-            .default_service(web::route().to(service::default_api))
-            .service(web::scope("/consumer")
-                     .service(service::consumer::basic::register)
-                     .service(service::consumer::basic::login)
-                     .service(service::consumer::basic::change_password)
-                     .service(service::consumer::basic::reset_password)
-                     .service(service::consumer::basic::modify_info))
+                .default_service(web::route().to(service::default_api))
+                .service(
+                    web::scope("/consumer")
+                        .service(service::consumer::register)
+                        .service(service::consumer::login)
+                        .service(service::consumer::modify_info)
+                        .service(service::consumer::change_password)
+                )
         }
     )
-    .bind("0.0.0.0:8000")?
-    .run()
-    .await
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
